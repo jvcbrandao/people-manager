03699e64df83ffbac4e71c50c9ae8ba914d430ee
João Vitor <jvcbrandaomat@gmail.com>
2025-11-30 11:03:11 -0300

    chore: Versão com CRUD funcionando

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
 onload = () => {
-  const bodyTable = document.getElementById("bodyTable");
+  listarPessoas();
+};
 
+let editId = null;
+
+const listBtnExcluir = document.getElementsByClassName("btnExcluir");
+
+document
+  .getElementById("cadastrarUsuario")
+  .addEventListener("submit", salvarUsuario);
+
+function listarPessoas() {
+  const bodyTable = document.getElementById("bodyTable");
   fetch("http://localhost:8000/api/pessoas.php", {
     method: "GET",
   })
@@ -8,7 +19,6 @@ onload = () => {
     .then((data) =>
       data.map((d) => {
         bodyTable.innerHTML += `
-
                     <tr>
                       <td>${d.id}</td>
                       <td>${d.nome}</td>
@@ -16,34 +26,32 @@ onload = () => {
                       <td>${d.idade}</td>
                       <td>${d.data_criacao}</td>
                       <td>
-                        <button class="btnPrimario">Editar</button>
-                        <button class="btnTerciario">Excluir</button>
-
+                        <button class="btnPrimario btnEditar"  data-id="${d.id}">Editar</button>
+                        <button class="btnTerciario btnExcluir" data-id="${d.id}">
+                          Excluir
+                        </button>
                       </td>
                     </tr>
     `;
       })
     );
-};
+}
 
-document
-  .getElementById("cadastrarUsuario")
-  .addEventListener("submit", salvarUsuario);
-
-
-
-
-
-  
 function salvarUsuario(e) {
   e.preventDefault();
-
   let nome = document.getElementById("nome").value;
   let cpf = document.getElementById("cpf").value;
   let idade = document.getElementById("idade").value;
   let messageContainer = document.getElementById("messageContainer");
-  fetch("http://localhost:8000/api/pessoas.php", {
-    method: "POST",
+
+  const url = editId
+    ? `http://localhost:8000/api/pessoas.php?id=${editId}`
+    : "http://localhost:8000/api/pessoas.php";
+
+  const method = editId ? "PUT" : "POST";
+
+  fetch(url, {
+    method: method,
     headers: {
       "Content-Type": "application/json",
     },
@@ -62,8 +70,87 @@ function salvarUsuario(e) {
       } else {
         messageContainer.innerHTML = `<p class="msgErro">Resposta inesperada do servidor.</p>`;
       }
+
+      document.getElementById("cadastrarUsuario").reset();
+      editId = null;
+      bodyTable.innerHTML = "";
+
+      const salvarBtn = document.getElementById("salvarInfo");
+      if (salvarBtn) {
+        salvarBtn.textContent = "Salvar";
+      }
+      listarPessoas();
     })
     .catch((err) => {
       messageContainer.innerHTML = `<p class="msgErro">Erro: ${err}</p>`;
     });
 }
+
+const bodyTable = document.getElementById("bodyTable");
+
+bodyTable.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btnExcluir")) {
+    const id = e.target.dataset.id; // pega o data-id
+    excluirUsuario(id, e.target);
+  }
+
+  if (e.target.classList.contains("btnEditar")) {
+    const id = e.target.dataset.id; // pega o data-id
+    editarUsuario(id, e.target);
+  }
+});
+
+function excluirUsuario(id, botao) {
+  if (!confirm("Tem certeza que deseja excluir este usuário?")) return;
+
+  fetch(`http://localhost:8000/api/pessoas.php?id=${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const linha = botao.closest("tr");
+      linha.remove();
+    })
+    .catch((err) => {
+      console.error("Erro ao excluir:", err);
+      alert("Não foi possível excluir o usuário.");
+    });
+}
+
+function listarPessoa(id) {
+  let nome = document.getElementById("nome");
+  let cpf = document.getElementById("cpf");
+  let idade = document.getElementById("idade");
+  const bodyTable = document.getElementById("bodyTable");
+  fetch(`http://localhost:8000/api/pessoas.php?id=${id}`, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      nome.value = data.nome;
+      cpf.value = data.cpf;
+      idade.value = data.idade;
+    });
+}
+
+function editarUsuario(id, botao) {
+  editId = id;
+  listarPessoa(id);
+
+  const salvarBtn = document.getElementById("salvarInfo");
+  if (salvarBtn) {
+    salvarBtn.textContent = "Atualizar";
+  }
+
+}
+
+// const pesquisarCpf = document.getElementById('pesquisarCPF');
+// pesquisarCpf.addEventListener('click', pesquisarCpf)
+
+// function pesquisarCpf(){
+
+// cpf = pesquisarCpf.value;
+
+
+// }
